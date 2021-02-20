87c332b6d6e8284b6db439d22d32b4258e94d7c0
TechCare <buitrancusi@gmail.com>
2021-02-20 15:50:44 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Đời không tâm điểm",
+    img: "img/doikotamdiem.jpg",
+    price: 10,
+    company: "Nha Xb VĂN HỌC",
+    info:
+      "Đời không tâm điểm ,tác giả Krishnamurti, Nguyễn Ước dịch, nhà xb văn hoc 2005,sách không bán , ai muốn đọc xin liên hệ Mr.Hồng",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Khai sáng trí năng",
+    img: "img/khaisangtrinang.jpg",
+    price: 16,
+    company: "Nhà xuất bản văn hóa",
+    info:
+      "Khai sáng trí năng ,tác giả Krishnamurti,  nhà xb văn hóa dân tộc,sách không bán , ai muốn đọc xin liên hệ Mr.Hồng",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Kinh dịch",
+    img: "img/kinhdich.jpg",
+    price: 8,
+    company: "nhà xb KHXH",
+    info:
+      "Sọan giả Dương ngọc Dũng , lê anh Minh, bàn về Cấu hình tư tưởng Trung quốc, sách không bán, muốn mượn đọc thì liên hê mr. hồng ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Sư tử tuyết bờm xanh",
+    img: "img/sututuyet.jpg",
+    price: 18,
+    company: "nhà xb tp Hồ chí Minh",
+    info:
+      "Nguyễn Tường Bách dịch",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Tam Thiên Tự",
+    img: "img/tamthientu.jpg",
+    price: 24,
+    company: "nxb Trí Đức Tòng Thơ",
+    info:
+      "Sách học chữ Hán của người xưa, Đoàn Trung Còn dịch",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Thi nhân Việt Nam",
+    img: "img/thinhanvn.jpg",
+    price: 17,
+    company: "nhà xb Văn học",
+    info:
+      "Các nhà thơ Việt nam giai đoạn trước 1975,  Hoài Thanh và Hoài Chân tuyển chọn và bình luận",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Trốn Chạy",
+    img: "img/tronchay.jpg",
+    price: 30,
+    company: "nxb Văn Học",
+    info:
+      "Tập truyện ngắn của tác giả Alice Munro",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Bí quyết vẽ tranh sơn dầu",
+    img: "img/vesondau.jpg",
+    price: 2,
+    company: "nxb Mỹ Thuật",
+    info:
+      "Kỷ thuật vẽ tranh sơn dầu cho người mới hoc, tác giả Huỳnh Phạm Hương Trang",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Đời không tâm điểm",
+  img: "img/doikotamdiem.jpg",
+  price: 10,
+  company: "nhà xb văn học",
+  info:
+  "Đời không tâm điểm ,tác giả Krishnamurti, Nguyễn Ước dịch, nhà xb văn hoc 2005,sách không bán , ai muốn đọc xin liên hệ Mr.Hồng",
+  inCart: false,
+  count: 0,
+  total: 0
+};
